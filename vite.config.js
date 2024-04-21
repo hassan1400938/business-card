7ffd6788c96ffa54916f5f037390f905215fc7e6
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2024-04-21 10:30:37 +0500

    correct urls

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MyDigitalBusinessCard/",
+  base: "/my-digital-business-card/",
   plugins: [react()],
 });
